6d4a4bb7c6a09817e6ff17e26ce6aab2487d6ce2
ИС201-1 <IS201-1@tisbi.ru>
2023-11-15 17:04:41 +0300

    First Commit

---
+++ SaifutdinovPractic1/SaifutdinovPractic1/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using SaifutdinovPractic1.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+namespace SaifutdinovPractic1
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static SaifutdinovWPFEntities DB = new SaifutdinovWPFEntities();
+    }
+}
